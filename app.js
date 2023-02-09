67cc5387c0ef26bff19ea032ad43c2b3f2153cd8
Marie Guigou <guigoumarie@gmail.com>
2023-02-09 14:46:11 +0100

    create a cluster on mongoDB, install Mongoose and connect to the database

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use((req, res) => {
   res.json({ message: "Votre requête a été reçue" });
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://MGuigou:Jude111112@cluster0.zlv4t4d.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 module.exports = app;
