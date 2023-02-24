1901e54795d4aae99fee354ebc8bf979d5e27a4e
Marie Guigou <guigoumarie@gmail.com>
2023-02-24 11:49:18 +0100

    get routes

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -28,6 +28,14 @@ exports.createSauce = (req, res, next) => {
     });
 };
 
-// exports.getSauce = (req, res, next) => {
+exports.getAllSauce = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
 
-// };
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
